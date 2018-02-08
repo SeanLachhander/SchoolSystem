3201cbd841f951ca033a216ba71fd1d9df4aa6f1
seanlachhander <seanlachhander@gmail.com>
2018-02-07 20:24:41 -0500

    Added functionality.

--- src/Admin/AdminController.java
+++ src/Admin/AdminController.java
@@ -1,4 +1,82 @@
 package Admin;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import dbUtil.dbConnection;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.sql.Connection;
+
+
 public class AdminController {
+
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField firstname;
+
+    @FXML
+    private TextField lastname;
+    @FXML
+    private TextField email;
+    @FXML
+    private DatePicker dob;
+
+    @FXML
+    private TableView<StudentData> studenttable;
+
+    @FXML
+    private TableColumn<StudentData, String> idcolumn;
+
+    @FXML
+    private TableColumn<StudentData, String> firstnamecolumn;
+
+    @FXML
+    private TableColumn<StudentData, String> lastnamecolumn;
+
+    @FXML
+    private TableColumn<StudentData, String> emailcolumn;
+
+    @FXML
+    private TableColumn<StudentData, String> dobcolumn;
+
+    private dbConnection dc;
+    private ObservableList<StudentData> data;
+
+    private String sql = "SELECT * FROM students";
+    public void initialize(URL url, ResourceBundle rb){
+        this.dc = new dbConnection();
+
+    }
+
+    @FXML
+    private void loadStudentData(ActionEvent event){
+        try{
+            Connection conn = dbConnection.getConnection();
+            this.data = FXCollections.observableArrayList();
+
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            while(rs.next()){
+                this.data.add(new StudentData(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)));
+            }
+        }
+        catch(SQLException e){
+            System.err.println("Error" + e);
+        }
+    }
+
 }
+
+
+
+
+
